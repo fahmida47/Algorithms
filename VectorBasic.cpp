96123b4145e441eb759318091f20d7389a3144eb
Fahmida Afrin Nadia <fahmida.200547@gmail.com>
2026-01-18 10:28:42 +0600

    Enhance vector examples with additional operations
    Added examples for vector operations including front() and iterating through elements.

--- VectorBasic.cpp
+++ VectorBasic.cpp
@@ -79,7 +79,39 @@ int main()
      v.push_back(3);//v[2];
      v.push_back(4);//v[3];
      v.push_back(5);//v[4];
-    
-    cout<<v.size()<<endl;
+
+     cout<<v.size()<<endl;
+     cout<<" Another way "<<endl;
+     for(int i=0;i<v.size();i++)
+     {
+         cout<<v[i]<<"  ";
+     }
+     cout<<endl;
 }
+
+Topic- 05
+//------------------------------ Front()-----------------------------------//
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     vector<int>v;
+     v.push_back(1); //v[0];
+     v.push_back(2);//v[1];
+     v.push_back(3);//v[2];
+     v.push_back(4);//v[3];
+     v.push_back(5);//v[4];
+
+     for(int i=0;i<v.size();i++)
+     {
+         cout<<v[i]<<"  ";
+     }
+     cout<<endl;
+
+     cout<<v.front()<<endl;
+
+}
+
+
+
 
